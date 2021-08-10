4a4e452a39284575cd9f73141b4ed3ffb951cda1
Clem S-K <64632596+ClemSK@users.noreply.github.com>
2021-08-10 18:16:45 +0100

    react project

---
+++ react-static-hw/src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    
+      <nav>
+        <ul>
+          <li>
+            About
+          </li>
+          <li>
+            Menu
+          </li>
+          <li>
+            Gallery
+          </li>
+          <li>
+            Home
+          </li>
+        </ul>
+      </nav>
+  )
+}
+
+export default NavBar
